42cf0469c75b3efd7870cbc686a21e454af8ae7c
Anchal Agrawal <157994728+anchalagr11@users.noreply.github.com>
2024-12-12 11:02:33 -0500

    Add files via upload

---
+++ Models/Unstructured_Weight_Pruning/Prune.py
@@ -0,0 +1,199 @@
+from __future__ import print_function
+
+import argparse
+import os
+import random
+import shutil
+import time
+
+import torch
+import torch.nn as nn
+import torch.nn.parallel
+import torch.backends.cudnn as cudnn
+import torch.optim as optim
+import torch.utils.data as data
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+
+from resnet50 import resnet
+
+
+parser = argparse.ArgumentParser(description='PyTorch CIFAR10/100 Training')
+# Datasets
+parser.add_argument('-d', '--dataset', default='cifar10', type=str)
+parser.add_argument('-j', '--workers', default=4, type=int, metavar='N',
+                    help='number of data loading workers (default: 4)')
+# Optimization options
+parser.add_argument('--epochs', default=300, type=int, metavar='N',
+                    help='number of total epochs to run')
+parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+                    help='manual epoch number (useful on restarts)')
+parser.add_argument('--train-batch', default=128, type=int, metavar='N',
+                    help='train batchsize')
+parser.add_argument('--test-batch', default=100, type=int, metavar='N',
+                    help='test batchsize')
+parser.add_argument('--lr', '--learning-rate', default=0.1, type=float,
+                    metavar='LR', help='initial learning rate')
+parser.add_argument('--drop', '--dropout', default=0, type=float,
+                    metavar='Dropout', help='Dropout ratio')
+parser.add_argument('--schedule', type=int, nargs='+', default=[150, 225],
+                        help='Decrease learning rate at these epochs.')
+parser.add_argument('--gamma', type=float, default=0.1, help='LR is multiplied by gamma on schedule.')
+parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                    help='momentum')
+parser.add_argument('--weight-decay', '--wd', default=1e-4, type=float,
+                    metavar='W', help='weight decay (default: 1e-4)')
+parser.add_argument('--resume', default='', type=str, metavar='PATH',
+                    help='path to latest checkpoint (default: none)')
+# Architecture
+parser.add_argument('--arch', '-a', metavar='ARCH', default='resnet20',
+                    choices=model_names,
+                    help='model architecture: ' +
+                        ' | '.join(model_names) +
+                        ' (default: resnet18)')
+parser.add_argument('--depth', type=int, default=29, help='Model depth.')
+parser.add_argument('--cardinality', type=int, default=8, help='Model cardinality (group).')
+parser.add_argument('--widen-factor', type=int, default=4, help='Widen factor. 4 -> 64, 8 -> 128, ...')
+parser.add_argument('--growthRate', type=int, default=12, help='Growth rate for DenseNet.')
+parser.add_argument('--compressionRate', type=int, default=1, help='Compression Rate (theta) for DenseNet.')
+# Miscs
+parser.add_argument('--manualSeed', type=int, help='manual seed')
+parser.add_argument('-e', '--evaluate', dest='evaluate', action='store_true',
+                    help='evaluate model on validation set')
+
+parser.add_argument('--save_dir', default='test_checkpoint/', type=str)
+parser.add_argument('--percent', default=0.6, type=float, help='percentage of weight to prune')
+
+args = parser.parse_args()
+state = {k: v for k, v in args._get_kwargs()}
+
+# Validate dataset
+assert args.dataset == 'cifar10' or args.dataset == 'cifar100', 'Dataset can only be cifar10 or cifar100.'
+
+# Use CUDA
+use_cuda = torch.cuda.is_available()
+
+# Random seed
+if args.manualSeed is None:
+    args.manualSeed = random.randint(1, 10000)
+random.seed(args.manualSeed)
+torch.manual_seed(args.manualSeed)
+if use_cuda:
+    torch.cuda.manual_seed_all(args.manualSeed)
+
+best_acc = 0  # best test accuracy
+
+def main():
+    global best_acc
+    start_epoch = args.start_epoch  # start from epoch 0 or last checkpoint epoch
+
+    if not os.path.isdir(args.save_dir):
+        mkdir_p(args.save_dir)
+
+    # Data
+    print('==> Preparing dataset %s' % args.dataset)
+    transform_train = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+
+    transform_test = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+    ])
+    if args.dataset == 'cifar10':
+        dataloader = datasets.CIFAR10
+        num_classes = 10
+    else:
+        dataloader = datasets.CIFAR100
+        num_classes = 100
+
+
+    trainset = dataloader(root='./data', train=True, download=True, transform=transform_train)
+    trainloader = data.DataLoader(trainset, batch_size=args.train_batch, shuffle=True, num_workers=args.workers)
+
+    testset = dataloader(root='./data', train=False, download=False, transform=transform_test)
+    testloader = data.DataLoader(testset, batch_size=args.test_batch, shuffle=False, num_workers=args.workers)
+
+    # Model
+    model = resnet(depth = depth, dataset = dataset)
+
+
+    model = torch.nn.DataParallel(model).cuda()
+    model.cuda()
+    cudnn.benchmark = True
+    print('    Total params: %.2fM' % (sum(p.numel() for p in model.parameters())/1000000.0))
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
+
+
+    #pruning 
+    total = 0
+    for m in model.modules():
+        if isinstance(m, nn.Conv2d):
+            total += m.weight.data.numel()
+    conv_weights = torch.zeros(total)
+    index = 0
+    for m in model.modules():
+        if isinstance(m, nn.Conv2d):
+            size = m.weight.data.numel()
+            conv_weights[index:(index+size)] = m.weight.data.view(-1).abs().clone()
+            index += size
+
+    y, i = torch.sort(conv_weights)
+    thre_index = int(total * args.percent)
+    thre = y[thre_index]
+    pruned = 0
+    print('Pruning threshold: {}'.format(thre))
+    zero_flag = False
+    for k, m in enumerate(model.modules()):
+        if isinstance(m, nn.Conv2d):
+            weight_copy = m.weight.data.abs().clone()
+            mask = weight_copy.gt(thre).float().cuda()
+            pruned = pruned + mask.numel() - torch.sum(mask)
+            m.weight.data.mul_(mask)
+            if int(torch.sum(mask)) == 0:
+                zero_flag = True
+            print('layer index: {:d} \t total params: {:d} \t remaining params: {:d}'.
+                format(k, mask.numel(), int(torch.sum(mask))))
+    print('Total conv params: {}, Pruned conv params: {}, Pruned ratio: {}'.format(total, pruned, pruned/total))
+
+    
+def test():
+    model.eval()
+    test_loss = 0
+    correct = 0
+    for data, target in test_loader:
+        if args.cuda:
+            data, target = data.cuda(), target.cuda()
+        data, target = Variable(data, volatile=True), Variable(target)
+        output = model(data)
+        test_loss += F.cross_entropy(output, target, size_average=False).data[0] # sum up batch loss
+        pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
+        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+
+    test_loss /= len(test_loader.dataset)
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.1f}%)\n'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    return correct / float(len(test_loader.dataset))
+
+def save_checkpoint(state, is_best, filepath):
+    torch.save(state, os.path.join(filepath, 'checkpoint.pth.tar'))
+    if is_best:
+        shutil.copyfile(os.path.join(filepath, 'checkpoint.pth.tar'), os.path.join(filepath, 'model_best.pth.tar'))
+
+num_parameters = sum([param.nelement() for param in model.parameters()])
+print(model)
+acc = test(model)
+
+
+def save_checkpoint(state, is_best, checkpoint, filename='pruned.pth.tar'):
+    filepath = os.path.join(checkpoint, filename)
+    torch.save(state, filepath)
+
+if __name__ == '__main__':
+    main()
